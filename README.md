71bbda5e00357f5d62d7c37b5546fe46b5289771
evelyncaraujo <evyaraujoifc@gmail.com>
2023-03-13 16:51:18 -0300

    c2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# atvd
+# exercweb
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
+  import{ ref } from 'vue' 
     const nome = 'Evelyn'
     const idade = 16
+    const contador = ref(0)
 
     function inverter(texto) {
         return texto.split('').reverse().join('')
+
     }
 
     function saudacao() {
         return `Olá, ${nome}!`
     }
-   
 </script>
 
 <template>
@@ -20,20 +22,31 @@
         <hr />
         <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
         <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome) }} </p>
         <p>O nome invertido é: {{ (inverter(nome)).toUpperCase()}} </p>
         <hr />
         <p> Exemplo de saudação usando função</p>
         <p> {{ saudacao() }}  </p>
     </div>
-     
-  
+    <div class="info">
+      contador:{{ contador }}
+      <button @click="contador++">+</button>
+      <button @click="contador--">-</button>
+      <div v-if="contador> 10" class="verde">E maior que 10</div>
+    <div v-else-if="contador <10" class="vermelho">é menor que 10</div>
+    <div v-else class="amarelo">E 10 </div>
+    </div>
+    
+
 </template>
+
 <style scoped>
 .info {
   background-color: rgba(35,12,22,1);
   padding: 20px 30px;
   color: rgb(183, 210, 219);
   border-radius: 10px;
+  margin: 0 20px;
 }
 
 h1 {
@@ -41,4 +54,14 @@ h1 {
   font-weight: bold;
   font-size: 1.5em;
 }
+.verde{
+    color: green;
+}
+.vermelho{
+    color: red;
+}
+.amarelo{
+    color: yellow;
+}
+
 </style>
